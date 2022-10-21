1454e50dd1f51be9b2c9153f3296009a9be7b038
JDigabri <johndigabriele@gmail.com>
2022-10-21 18:03:53 -0400

    second commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,19 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+
+fn greet(name: &str) -> String {
+    let my_int: i32 = name.parse().unwrap();
+    format!("{}", my_int + 1)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/components/Greet.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+
+const greetMsg = ref("");
+let name = ref("0");
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsg.value = await invoke("greet", { name: name.value });
+  name = greetMsg;
+}
+</script>
+
+<template>
+  <div class="card">
+    <button type="" @click="greet()">
+        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
+      </button>
+  </div>
+  <h1>{{ greetMsg }}</h1>
+</template>
+
